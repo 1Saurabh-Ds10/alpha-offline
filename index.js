8964a1227780cc3bdc2fed37bff26e90c9b5a8b0
Saurabh <1saurabh.desai@gmail.com>
2018-09-09 14:11:59 +0530

    downloaded dependecies and project setup

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import './src/config/ReactronConfig';
 
-AppRegistry.registerComponent('alphaOffline', () => App);
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+import {name as appName} from './app.json';
+
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+console.ignoredYellowBox = ['Remote debugger'];
+
+
+
+AppRegistry.registerComponent(appName, () => App);
